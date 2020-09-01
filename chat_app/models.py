1b3ce1d0bc128735449597565ec54b37c7076195
Devansh Soni <devanshsoni108@gmail.com>
2020-09-01 11:42:52 +0530

    Signals

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
 
 # Create your models here.
+
+class notify(models.Model):
+    title = models.TextField()
+    notification = models.TextField()
+
+def show_notify(sender, instance, **kwargs):
+    print("Something is there")
+    print(sender)
+    print(instance)
+
+post_save.connect(show_notify, sender = notify)
